e4c6a9d0828ac4c04b0b9769d15961ae12f913f7
vcds1029 <vcds1029@gmail.com>
2025-03-09 12:57:46 +0900

    투사체 멀티플 적용
    투사체 멀티플 적용

--- Assets/Scripts/ItemEffect/Magnetic.cs
+++ Assets/Scripts/ItemEffect/Magnetic.cs
@@ -2,24 +2,22 @@ using UnityEngine;
 
 public class Magnetic : MonoBehaviour
 {
-    [SerializeField] float radius;
-    [SerializeField] LayerMask layerMask;
+    [SerializeField] float radius; // 자석 범위
+    [SerializeField] LayerMask layerMask; // 감지할 레이어
+    [SerializeField] float powerMultiplier = 1.0f; // 자석의 계수
 
-    //타겟 당기기
+    // 타겟 당기기
     public void Pull()
     {
-        //스캔된 오브젝트들
+        // 스캔된 오브젝트들
         RaycastHit2D[] targets2D = Physics2D.CircleCastAll(transform.position, radius, Vector2.up, 0, layerMask);
-        
-        
-        //Debug.Log(targets2D.Length);
 
         foreach (RaycastHit2D target2D in targets2D)
         {
             if (target2D.transform.CompareTag("Coin"))
             {
                 Vector2 dir = transform.position - target2D.transform.position;
-                float pullPower = dir.magnitude;
+                float pullPower = dir.magnitude * powerMultiplier;  // 힘 배율 적용
                 dir = dir.normalized * pullPower;
 
                 Rigidbody2D _rigid2D = target2D.transform.GetComponent<Rigidbody2D>();
